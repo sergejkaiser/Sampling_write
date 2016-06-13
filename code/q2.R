b2106cddc09bb3210de3c9355cc3b4348627fce0
xzork <xzork@users.noreply.github.com>
2016-06-13 21:18:15 +0200

    Update q2.R

--- code/q2.R
+++ code/q2.R
@@ -1,3 +1,4 @@
+# for q2.
 HIS <- read.csv('../data/bmi_voeg.csv')
 HIS$region <- factor(HIS$region, levels=1:3, labels=c("Flanders", "Brussels", "Wallonia"))
 HIS$province <- factor(HIS$province, levels=1:12, labels=c("Antwerpen", "Vlaams-Brabant", "Limburg",
